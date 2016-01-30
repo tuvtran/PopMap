f8bb7faa245f2b1c5234af8b2227ae6d241cc678
Robert Gardner <rhg259@nyu.edu>
2016-01-30 10:30:00 -0500

    Refactor inputs and MapView

--- index.ios.js
+++ index.ios.js
@@ -16,195 +16,53 @@ import React, {
   View
 } from 'react-native';
 
-//class AwesomeProject extends Component {
-  //constructor(props) {
-    //super(props);
-    //this.state = {
-      //origin: '',
-      //destination: '',
-    //};
-  //}
-
-  //render() {
-    //return (
-      //<View style={styles.container}>
-        //<View style={styles.row}>
-          //<Text>
-            //{'Origin'}
-          //</Text>
-          //<TextInput
-            //onChangeText={(text) => this.setState({origin: text})}
-            //onSubmitEditing={(text) => this.refs.DestinationInput.focus()}
-            //returnKeyType={'next'}
-            //style={styles.textInput}
-            //value={this.state.origin}/>
-          //<TextInput />
-        //</View>
-        //<View style={styles.row}>
-          //<Text>
-            //{'Destination'}
-          //</Text>
-          //<TextInput
-            //ref='DestinationInput'
-            //onChangeText={(text) => this.setState({destination: text})}
-            //onSubmitEditing={(_) => this.requestRoute()}
-            //returnKeyType='go'
-            //style={styles.textInput}
-            //value={this.state.destination}/>
-          //<TextInput />
-        //</View>
-        //<View style={styles.row}>
-          //<MapView
-            //initialRegion={{
-              //latitude: 38.78825,
-              //longitude: -122.4324,
-              //latitudeDelta: 0.0922,
-              //longitudeDelta: 0.0421,
-            //}}
-          ///>
-        //</View>
-      //</View>
-    //);
-  //}
-
-  //requestRoute() {
-    //console.log('origin: ' + this.state.origin);
-    //console.log('destination: ' + this.state.destination);
-  //}
-//}
-
-var regionText = {
-  latitude: '0',
-  longitude: '0',
-  latitudeDelta: '0',
-  longitudeDelta: '0',
-};
-
-var MapRegionInput = React.createClass({
-
-  propTypes: {
-    region: PropTypes.shape({
-      latitude: PropTypes.number.isRequired,
-      longitude: PropTypes.number.isRequired,
-      latitudeDelta: PropTypes.number,
-      longitudeDelta: PropTypes.number,
-    }),
-    onChange: PropTypes.func.isRequired,
-  },
-
-  getInitialState() {
-    return {
-      region: {
-        latitude: 0,
-        longitude: 0,
-      }
+class RouteInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      origin: '',
+      destination: '',
     };
-  },
+  }
 
-  componentWillReceiveProps: function(nextProps) {
-    this.setState({
-      region: nextProps.region || this.getInitialState().region
-    });
-  },
-
-  render: function() {
-    var region = this.state.region || this.getInitialState().region;
+  render() {
     return (
       <View style={styles.container}>
         <View style={styles.row}>
-          <Text>
-            {'Latitude'}
-          </Text>
           <TextInput
-            value={'' + region.latitude}
+            onChangeText={(text) => this.setState({origin: text})}
+            onSubmitEditing={(text) => this.refs.DestinationInput.focus()}
+            placeholder={'Origin'}
+            returnKeyType={'next'}
             style={styles.textInput}
-            onChange={this._onChangeLatitude}
-            selectTextOnFocus={true}
-          />
+            value={this.state.origin} />
         </View>
         <View style={styles.row}>
-          <Text>
-            {'Longitude'}
-          </Text>
           <TextInput
-            value={'' + region.longitude}
+            ref='DestinationInput'
+            onChangeText={(text) => this.setState({destination: text})}
+            onSubmitEditing={(_) => this.requestRoute()}
+            placeholder={'Destination'}
+            returnKeyType='route'
             style={styles.textInput}
-            onChange={this._onChangeLongitude}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.row}>
-          <Text>
-            {'Latitude delta'}
-          </Text>
-          <TextInput
-            value={
-              region.latitudeDelta == null ? '' : String(region.latitudeDelta)
-            }
-            style={styles.textInput}
-            onChange={this._onChangeLatitudeDelta}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.row}>
-          <Text>
-            {'Longitude delta'}
-          </Text>
-          <TextInput
-            value={
-              region.longitudeDelta == null ? '' : String(region.longitudeDelta)
-            }
-            style={styles.textInput}
-            onChange={this._onChangeLongitudeDelta}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.changeButton}>
-          <Text onPress={this._change}>
-            {'Change'}
-          </Text>
+            value={this.state.destination} />
         </View>
       </View>
     );
-  },
+  }
 
-  _onChangeLatitude: function(e) {
-    regionText.latitude = e.nativeEvent.text;
-  },
+  requestRoute() {
+    console.log('origin: ' + this.state.origin);
+    console.log('destination: ' + this.state.destination);
+  }
+}
 
-  _onChangeLongitude: function(e) {
-    regionText.longitude = e.nativeEvent.text;
-  },
-
-  _onChangeLatitudeDelta: function(e) {
-    regionText.latitudeDelta = e.nativeEvent.text;
-  },
-
-  _onChangeLongitudeDelta: function(e) {
-    regionText.longitudeDelta = e.nativeEvent.text;
-  },
-
-  _change: function() {
-    this.setState({
-      region: {
-        latitude: parseFloat(regionText.latitude),
-        longitude: parseFloat(regionText.longitude),
-        latitudeDelta: parseFloat(regionText.latitudeDelta),
-        longitudeDelta: parseFloat(regionText.longitudeDelta),
-      },
-    });
-    this.props.onChange(this.state.region);
-  },
-
-});
-
-var MapViewExample = React.createClass({
+var SafeRoute = React.createClass({
 
   getInitialState() {
     return {
       isFirstLoad: true,
       mapRegion: undefined,
-      mapRegionInput: undefined,
       annotations: [],
     };
   },
@@ -212,10 +70,7 @@ var MapViewExample = React.createClass({
   render() {
     return (
       <View>
-        <MapRegionInput
-          onChange={this._onRegionInputChanged}
-          region={this.state.mapRegionInput}
-        />
+        <RouteInput />
         <MapView
           showsUserLocation={true}
           style={styles.map}
@@ -228,43 +83,11 @@ var MapViewExample = React.createClass({
     );
   },
 
-  _getAnnotations(region) {
-    return [{
-      longitude: region.longitude,
-      latitude: region.latitude,
-      title: 'You Are Here',
-    }];
-  },
-
-  _onRegionChange(region) {
-    this.setState({
-      mapRegionInput: region,
-    });
-  },
-
-  _onRegionChangeComplete(region) {
-    if (this.state.isFirstLoad) {
-      this.setState({
-        mapRegionInput: region,
-        annotations: this._getAnnotations(region),
-        isFirstLoad: false,
-      });
-    }
-  },
-
-  _onRegionInputChanged(region) {
-    this.setState({
-      mapRegion: region,
-      mapRegionInput: region,
-      annotations: this._getAnnotations(region),
-    });
-  },
-
 });
 
 var styles = StyleSheet.create({
   map: {
-    height: 150,
+    height: 300,
     margin: 10,
     borderWidth: 1,
     borderColor: '#000000',
@@ -277,20 +100,14 @@ var styles = StyleSheet.create({
     paddingTop: 100,
   },
   textInput: {
-    width: 150,
+    width: 250,
     height: 20,
     borderWidth: 0.5,
     borderColor: '#aaaaaa',
     fontSize: 13,
     padding: 4,
-  },
-  changeButton: {
-    alignSelf: 'center',
-    marginTop: 5,
-    padding: 3,
-    borderWidth: 0.5,
-    borderColor: '#777777',
+    marginLeft: 10,
   },
 });
 
-AppRegistry.registerComponent('SafeRoute', () => MapViewExample);
+AppRegistry.registerComponent('SafeRoute', () => SafeRoute);
