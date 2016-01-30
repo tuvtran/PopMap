c8901fb9a9f5c116898930a04af2fa4f89443d6e
Robert Gardner <rhg259@nyu.edu>
2016-01-30 03:27:41 -0500

    Add and style two text inputs

--- index.ios.js
+++ index.ios.js
@@ -14,16 +14,6 @@ import React, {
   View
 } from 'react-native';
 
-var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
-var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
-var PAGE_SIZE = 25;
-var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
-var REQUEST_URL = API_URL + PARAMS;
-
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
-
 class AwesomeProject extends Component {
   constructor(props) {
     super(props);
@@ -34,19 +24,28 @@ class AwesomeProject extends Component {
   }
 
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(text) => this.setState({origin: text})}
-          value={this.state.origin}/>
-        <TextInput />
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(text) => this.setState({destination: text})}
-          value={this.state.destination}/>
-        <TextInput />
+        <View style={styles.row}>
+          <Text>
+            {'Origin'}
+          </Text>
+          <TextInput
+            onChangeText={(text) => this.setState({origin: text})}
+            style={styles.textInput}
+            value={this.state.origin}/>
+          <TextInput />
+        </View>
+        <View style={styles.row}>
+          <Text>
+            {'Destination'}
+          </Text>
+          <TextInput
+            onChangeText={(text) => this.setState({destination: text})}
+            style={styles.textInput}
+            value={this.state.destination}/>
+          <TextInput />
+        </View>
       </View>
     );
   }
@@ -54,29 +53,21 @@ class AwesomeProject extends Component {
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
     flex: 1,
+    paddingTop: 50,
   },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  year: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+  textInput: {
+    width: 150,
+    height: 20,
+    borderWidth: 0.5,
+    borderColor: '#aaaaaa',
+    fontSize: 13,
+    padding: 4,
   },
 });
 
